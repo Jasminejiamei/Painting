e5e865d45a622086bbec8bb427fa00113803a492
Jasminejiamei <1121663274@qq.com>
2018-10-31 00:15:03 +0800

    newsearch

--- js/accountManagement.js
+++ js/accountManagement.js
@@ -48,8 +48,60 @@
 			var pictureFile = document.getElementById("pictureFile");
 			pictureFile.click();
 		}
+		//编辑
+		$(".write").click(function(){
+			var bh = $("body").height();
+				var bw = $("body").width(); 
+				$("#fullbg").css({ 
+				height:bh, 
+				width:bw, 
+				display:"block" 
+				}); 
+				$("#dialogDescribe").show();
+			}) 
+		$('input[name=closeButtonD]').click(function(){
+				$("#fullbg,#dialogDescribe").hide(); 
+			})
+
+		//删除
+		   $(function(){
+				$("tbody>tr:odd").addClass("odd");
+				$("tbody>tr:even").addClass("even");
+			    //删除按钮点击事件
+			    $(".delete").click(function(){
+			     //获取按钮的父节点的父节点删除
+			     $(this).parent().parent().remove();
+			    });			    
+			     $("tr").mouseenter(function(){
+			     $(this).css({"opacity":1}).siblings().css({"opacity":1});
+			    });
+			     $("table").mouseleave(function(){
+			     $("tr").css({"opacity":1});
+			    });
+			   })
+
 		
-			
+
+		//添加
+		$(function() {
+			$(".sureButtonadd").click(function() {
+			var tr = "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>";
+			//$("table").append(tr);
+			$("table tr:eq(1)").before(tr);
+			});
+			});
+		$('input[name=sureButtonadd]').click(function(){
+			$("#fullbg,#dialog,#dialogSecond").hide(); 
+			})
+			   	
+//备注
+		$('.btnDropdown').click(function(){
+			$('li').show();
+		})
+		$('.btnDropdown').blur(function(){
+			$('li').hide();
+		})
+
 //表单认证
         $('input[name=username]').blur(function(){
                 val=$(this).val(); // 获取用户名
@@ -85,118 +137,17 @@
         console.log($('input[id=selectHead]')[0]);
         $(function(){
             $("#selectHead").click(function(){
-                $("input").prop("checked",this.checked);
+				$("input").prop("checked",this.checked);
+				
             });
         });
 
-// 分页
-    $().ready(function(){
-        var curPage =$("#currentPage").val();
-        var last =$("#totalPage").val();
-        var page =Math.ceil(curPage/10);//向上取整函数
-        //调用绘制分页样式函数
-        draw(page,curPage);
-
-        //绑定点击页码事件
-        $(document).on("click",".pagination li a",function(){
-            var str =$(this).html();
-            if(!isNaN(str)){
-                //移除之前的active
-                $(".pagination li a").removeClass("active");
-                $(this).attr("class","active");
-                $("#currentPage").val(str);
-            }
-	});
-	
-	//绑定下一页点击事件
-	$(document).on("click","#nextPage",function(){
-		var num =$(".active").html();
-		var curPage =$("#currentPage").val();
-		var last =$("#totalPage").val();
-		var page =Math.ceil((parseInt(num))/10);
-		if(num < page*10 && num != last){
-			//移除之前的active
-			$(".pagination li a").removeClass("active");
-			$("#"+(parseInt(num)+1)+"").attr("class","active");
-			//$(".pagination li a[text="+(parseInt(num)+1)+"]").attr("class","active");//jQ1.6支持
-			$("#currentPage").val(parseInt(num)+1);
-		}else if(num == page*10 && num != last){
-			//清空之前的数据
-			$(".pagination").html("");
-			draw(page+1,(parseInt(num)+1));
-		}
-	})
-	
- 
-	//绑定上一页点击事
-	$(document).on("click","#previousPage",function(){
-		var num =$(".active").html();
-		var curPage =$("#currentPage").val();
-		var last =$("#totalPage").val();
-		var page =Math.ceil((parseInt(num))/10);
-		if(num <= page*10 && num != (page-1)*10+1){
-			//移除之前的active
-			$(".pagination li a").removeClass("active");
-			$("#"+(parseInt(num)-1)+"").attr("class","active");
-			//$(".pagination li a[text="+(parseInt(num)+1)+"]").attr("class","active");//jQ1.6支持
-			$("#currentPage").val(parseInt(num)-1);
-		}else if(num == (page-1)*10+1 && num != 1){
-			//清空之前的数据
-			$(".pagination").html("");
-			draw(page-1,(parseInt(num)-1));
-		}
-	})
-	
-	//绘制页面分页样式
-	function draw(page,curPage){
-		//页面中的当前页
-		var page =page;
-		//后台传过来的页数
-		var curPage = curPage;
-		//后台传过来的总页数
-		var datas =$("#totalPage").val();
-		//每页显示多少条数据
-		var pageSize =5;
-		//在网页中一共要分多少页
-		var totalPage = Math.floor((datas-1)/pageSize+1);
+		$("tbody>tr").click(function(){
+			$(this)
+			.addClass('selected')
+			.siblings()
+			.removeClass('selected')
+			.end()
+			.find('checkbox').attr('checked',this.checked);
+		})
 		
-		var liStr ="<li><a id='previousPage' href='#'>«</a></li>";
-		$("#currentPage").val(curPage);
-		if(page <= totalPage){
-			if(datas <= 10){
-				for(i=1; i<=datas; i++){
-					//为当前页增加样式
-					var active ="";
-					if(i==curPage){
-						active=" class='active' ";
-					}
-					liStr +=" <li><a id="+i+" "+active+" href='#'>"+i+"</a></li>"
-				}
-			}else{
-				var start =pageSize*(page-1)+1;
-				var end =page*pageSize;
-				if(page == totalPage){
-					for(i=start; i<=datas; i++){
-						//为当前页增加样式
-						var active ="";
-						if(i==curPage){
-							active=" class='active' ";
-						}
-						liStr +=" <li><a id="+i+" "+active+" href='#'>"+i+"</a></li>"
-					}
-				}else{
-					for(i=start; i<=end; i++){
-						//为当前页增加样式
-						var active ="";
-						if(i==curPage){
-							active=" class='active' ";
-						}
-						liStr +=" <li><a id="+i+" "+active+" href='#'>"+i+"</a></li>"
-					}
-				}
-			}
-		}
-		liStr +="<li><a id='nextPage' href='#'>»</a></li>";
-		$(".pagination").append(liStr);
-	}
-})
